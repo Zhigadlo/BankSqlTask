bfad266f3070381a6006f45204916c7a4ec798a1
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2023-02-01 15:30:31 +0300

    Add triggers for insert and update
    Add update trigger for account  table and insert, update trigger for cards table

--- SQLTask.sql
+++ SQLTask.sql
@@ -33,6 +33,7 @@ create table Accounts
 	Id int primary key identity(1, 1),
 	ClientId int not null,
 	BankId int not null,
+	Balance money default 0,
 	constraint FK_Accounts_To_Clients foreign key(ClientId) references Clients(Id) on delete cascade,
 	constraint FK_Accounts_To_Banks foreign key(BankId) references Banks(Id) on delete cascade
 );
@@ -42,6 +43,7 @@ create table Cards
 	Id int primary key identity(1, 1),
 	Number varchar(16) not null,
 	AccountId int not null,
+	Balance money default 0,
 	constraint FK_Cards_To_Accounts foreign key(AccountId) references Accounts(Id) on delete cascade
 );
 
@@ -61,3 +63,60 @@ create table CityBankBranches
 	constraint FK_CityBankBranches_To_Cities foreign key(CityId) references Cities(Id) on delete cascade,
 	constraint FK_CityBankBranches_To_BankBranches foreign key(BankBranchId) references BankBranches(Id) on delete cascade
 );
+
+go
+
+create trigger Accounts_Update
+on Accounts 
+for update
+as
+begin 
+	if update(Balance)
+	begin
+		declare @AccountBalance money, @CardsAccountBalance money
+		
+		select @AccountBalance = inserted.Balance, @CardsAccountBalance = Sum(c.Balance)
+		from inserted
+		join Cards as c on inserted.Id = c.AccountId
+		group by inserted.Balance
+
+		if @AccountBalance < @CardsAccountBalance
+		begin
+			rollback transaction
+			print 'There is not enough money'
+		end
+	end
+end
+
+go
+
+create trigger Cards_Insert_Update
+on Accounts 
+for insert, update
+as
+begin 
+	
+	declare @AccountBalance money, @CardsAccountBalance money, @AccountId int
+	
+	open cur
+	fetch next into @AccountId
+	while @@fetch_status = 0
+	begin
+		select @AccountBalance = a.Balance, @CardsAccountBalance = Sum(c.Balance)
+		from Accounts as a
+		join Accounts as c on inserted.Account = c.Id
+		group by inserted.Balance
+
+		if @AccountBalance < @CardsAccountBalance
+		begin
+			rollback transaction
+			print 'You have not enough money on the card with id=' + convert(nvarchar, inserted.Id);
+		end
+
+		fetch next from cur into @AccountId
+	end
+end
+go
+
+
+
